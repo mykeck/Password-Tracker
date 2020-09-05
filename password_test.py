c83b3ac5fc4e2364d50ad9eb07b28badc6456bc0
MikeC <kiragumike@gmail.com>
2020-09-05 18:49:04 +0300

    added tests

--- password_test.py
+++ password_test.py
@@ -10,7 +10,33 @@ class TestUser(unittest.TestCase):
 
     def test_init(self):
         self.assertEqual(self.new_user.user_name, "Mike")
-        self.assertEqual(self.new_user.password, "Collins")
+        self.assertEqual(self.new_user.password, "#12345")
+
+    def test_create_account(self):
+        self.new_user.create_account()  
+
+        self.assertEqual(len(User.user_list), 1)  
+        
+    def test_save_multiple_account(self):
+        self.new_user.create_account() 
+
+        user1 = User('kimkim', '6rtef')
+        user1.create_account()
+
+        self.assertEqual(len(User.user_list), 2)
+
+    def test_account_exist(self):
+        self.new_user.create_account()
+
+        user2 = User('sally', '*999')
+        user2.create_account()  
+
+        user_exists = User.user_exists('sally', '*999')
+
+        self.assertTrue(user_exists) 
+
+        
+        # self.new_user_credential = User("denischom"") 
 
 if __name__ == '__main__':
     unittest.main()             
