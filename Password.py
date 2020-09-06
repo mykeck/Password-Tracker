52c6c15fa3d90a6a8790a81e10ce6846f7d5732e
MikeC <kiragumike@gmail.com>
2020-09-06 11:04:52 +0300

    run a user-exist method test

--- Password.py
+++ Password.py
@@ -7,3 +7,18 @@ class User:
 
     def create_account(self):
         User.user_list.append(self)
+
+    @classmethod
+    def user_exists(cls, user_name, password):
+        '''
+        method that checks if a user exists from the users list
+        Args:
+            user_name: to search if the user name exists
+            password: to search if the password exists
+        Returns: 
+            boolean: True or false depending if the user exists
+        '''
+        for user_name in cls.user_list:
+            if user_name == user_name and password == password:
+                return True 
+                          
--- password_test.py
+++ password_test.py
@@ -3,14 +3,14 @@ from Password import User
 
 class TestUser(unittest.TestCase):
     def setUp(self):
-        self.new_user = User("Mike", "Collins")
+        self.new_user = User("Mike", "12345")
 
     def tearDown(self):
         User.user_list = []   
 
     def test_init(self):
         self.assertEqual(self.new_user.user_name, "Mike")
-        self.assertEqual(self.new_user.password, "#12345")
+        self.assertEqual(self.new_user.password,  "12345")
 
     def test_create_account(self):
         self.new_user.create_account()  
@@ -36,7 +36,7 @@ class TestUser(unittest.TestCase):
         self.assertTrue(user_exists) 
 
         
-        # self.new_user_credential = User("denischom"") 
+        
 
 if __name__ == '__main__':
     unittest.main()             
